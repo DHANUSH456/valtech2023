0e35ca990faf4414be54a80642b4bc762fe6b0e3
DHANUSH456 <dhanush.v.2001@gmail.com>
2023-10-31 12:14:59 +0530

    Say Hello in my first Maven Project

--- first-maven-project/src/main/java/com/valtech/training/first_maven_project/App.java
+++ first-maven-project/src/main/java/com/valtech/training/first_maven_project/App.java
@@ -1,11 +1,11 @@
 package com.valtech.training.first_maven_project;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+	
+	public String sayHello() {
+		return "HelloWorld";
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
